e1122ddd6182f00b6ef9082e9f06107bdb095e53
Oscar Maldonado <oscarmaldonado@Oscars-MacBook-Pro.local>
2022-04-21 02:05:34 -0700

    initial controller set up

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using note_main_backend.Models;
+using note_main_backend.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,15 +23,52 @@ namespace note_main_backend.Controllers
         [HttpPost, Route("LogEntry/Post")]
         public IActionResult PostLogEntry([FromBody] LogEntryModel model)
         {
+            using(var context = new NoteDBContext(_config))
+            {
+                var Entry = new LogEntry();
+                Entry.ID = model.ID;
+                Entry.Is_Archived = false;
+                Entry.NoteID = Guid.NewGuid().ToString();
+                Entry.Note_Body = model.Note_Body;
+                Entry.Note_Header = model.Note_Header;
+                Entry.Date_Added = DateTime.Now;
+
+                context.Attach<LogEntry>(Entry);
+                context.Add<LogEntry>(Entry);
+                return Ok(context.SaveChanges());
+            }
             
-            return Ok();
         }
 
         [HttpPost, Route("ArchiveEntry")]
         public IActionResult Archive_Entry([FromBody] ArchiveRequest model)
         {
-            
-            return Ok();
+
+            using (var context = new NoteDBContext(_config))
+            {
+                var logEntry = context.Logs.First(x => x.ID == model.ID && x.NoteID == model.NoteID);
+
+                logEntry.Is_Archived = true;
+                
+                return Ok(context.SaveChanges());
+            }
+
+        }
+
+
+        [HttpPost, Route("Update/Body")]
+        public IActionResult NoteBodyUpdate([FromBody] NoteBodyUpdateRequest model)
+        {
+
+            using (var context = new NoteDBContext(_config))
+            {
+                var logEntry = context.Logs.First(x => x.ID == model.ID && x.NoteID == model.NoteID);
+
+                logEntry.Note_Body = model.Note_Body;
+
+                return Ok(context.SaveChanges());
+            }
+
         }
 
     }
---
+++ Models/NoteBodyUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace note_main_backend.Models
+{
+    public class NoteBodyUpdateRequest
+    {
+        public string Note_Body { get; set; }
+
+        public string ID { get; set; }
+
+        public string NoteID { get; set; }
+    }
+}
